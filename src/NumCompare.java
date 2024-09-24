ee96b9faf23b8c41c9c31f3c1fbc9509f321ebe3
mmaalin <maalin.622139@swl.k12.oh.us>
2024-09-24 08:54:18 -0400

    NOT FINISHED as of 8:54 9/24/24

--- src/NumCompare.java
+++ src/NumCompare.java
@@ -1,5 +1,36 @@
-public class Main {
+import java.util.Scanner;
+
+public class NumCompare {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner (System.in);
+
+        double num1;
+        double num2;
+
+        System.out.println("Hello - please input two numbers, separately.");
+
+        if (scan.hasNextDouble()) {
+            num1 = scan.nextDouble();
+        } else {
+            System.out.println("You have inputted an invalid response.");
+            System.exit(0);
+        }
+
+        if (scan.hasNextDouble()) {
+            num2 = scan.nextDouble();
+        } else {
+            System.out.println("You have inputted an invalid response.");
+            System.exit(0);
+        }
+
+        if (num1 < num2) {
+            System.out.println(num1 + " is less than " + num2 + " .");
+        } else if (num1>num2) {
+            System.out.println(num1 + " is greater than " + num2 + " .");
+        } else {
+            System.out.println(num1 + " and " + num2 + " are equal.");
+        }
+
     }
 }
